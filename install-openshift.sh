eaf641e6aec6cb269e632e50c2c42a80feaefc9a
arunvdsharma <arun.vd.sharma@gmail.com>
2019-04-15 16:57:45 +0530

    File to install openshift

---
+++ install-openshift.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## Default variables to use
+[ ! -d openshift-ansible ] && git clone https://github.com/openshift/openshift-ansible.git -b release-3.11 --depth=1
+
+ansible-playbook -i inventory.ini openshift-ansible/playbooks/prerequisites.yml
+ansible-playbook -i inventory.ini openshift-ansible/playbooks/deploy_cluster.yml
+
+htpasswd -b /etc/origin/master/htpasswd admin admin123
+oc adm policy add-cluster-role-to-user cluster-admin admin
