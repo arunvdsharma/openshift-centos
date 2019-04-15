7e3c3ca6c12562704142d9ec192ff8dcd339eda6
arunvdsharma <arun.vd.sharma@gmail.com>
2019-04-15 16:58:21 +0530

    Pre-requisites

---
+++ install-tools.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+## Default variables to use
+
+# install updates
+yum update -y
+
+# install the following base packages
+yum install -y  wget git zile nano net-tools docker-1.13.1\
+				bind-utils iptables-services \
+				bridge-utils bash-completion \
+				kexec-tools sos psacct openssl-devel \
+				httpd-tools NetworkManager \
+				python-cryptography python2-pip python-devel  python-passlib \
+				java-1.8.0-openjdk-headless "@Development Tools"
+
+#install epel
+yum -y install epel-release
+
+# Disable the EPEL repository globally so that is not accidentally used during later steps of the installation
+sed -i -e "s/^enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
+
+systemctl | grep "NetworkManager.*running" 
+if [ $? -eq 1 ]; then
+	systemctl start NetworkManager
+	systemctl enable NetworkManager
+fi
+
+# install the packages for Ansible
+yum -y --enablerepo=epel install pyOpenSSL
+
+#curl -o ansible.rpm https://releases.ansible.com/ansible/rpm/release/epel-7-x86_64/ansible-2.6.5-1.el7.ans.noarch.rpm
+#yum -y --enablerepo=epel install ansible.rpm
+
+yum -y --enablerepo=epel install https://releases.ansible.com/ansible/rpm/release/epel-7-x86_64/ansible-2.6.5-1.el7.ans.noarch.rpm
+
+mkdir -p /etc/origin/master/
+touch /etc/origin/master/htpasswd
