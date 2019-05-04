f68d56672d686a8c7f9d0335d5d828a1ea7e6f13
arunvdsharma <arun.vd.sharma@gmail.com>
2019-05-04 15:49:50 +0530

    Update install-tools.sh

--- install-tools.sh
+++ install-tools.sh
@@ -33,6 +33,3 @@ yum -y --enablerepo=epel install pyOpenSSL
 #yum -y --enablerepo=epel install ansible.rpm
 
 yum -y --enablerepo=epel install https://releases.ansible.com/ansible/rpm/release/epel-7-x86_64/ansible-2.6.5-1.el7.ans.noarch.rpm
-
-mkdir -p /etc/origin/master/
-touch /etc/origin/master/htpasswd
